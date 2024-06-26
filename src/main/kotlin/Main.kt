ddbd78a08a428c41f607b74a1027023adec5c072
Yazan <yazan.ma.aamer@gmail.com>
2024-06-26 12:26:37 +0400

    Updated readme.md

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,68 +1,26 @@
 package org.example
 
+import java.util.Scanner
+import kotlin.system.exitProcess
+
 fun main() {
-    val commandParser = CommandParser()
-    val commandValidator: CommandValidator = AddValidator()
-    val commands = commandParser.parseCommand(
-        "add this is a command test and this is the note --option2 --option2")
+    val scanner = Scanner(System.`in`)
+    val parser = CommandParser()
 
-    println(commandValidator.validate(commands))
+    while (true) {
+        print("> ")
+        val line = scanner.nextLine()
 
+        try {
+            val parsedCommand = parser.parseCommand(line)
+            val result = CommandsStore.validators.get(parsedCommand.commandName)?.validate(parsedCommand)
+            println(result)
+            println(parsedCommand)
+        } catch (e: Exception) {
+            println("Command note found!")
+            exitProcess(-1)
+        }
 
+    }
 
-
-
-
-//    val result2 = commandParser.parseCommand(
-//        "remove 1 --option --anotheroption")
-//
-//    val result3 = commandParser.parseCommand(
-//        "update 1 this is the updating string")
-//
-//    val result4 = commandParser.parseCommand(
-//        "list --option")
-
-    println(commands)
-
-//    val scanner = Scanner(System.`in`)
-//    val notes = mutableListOf<String>()
-//
-//    // Handling Ctrl+C to exit gracefully
-//    Runtime.getRuntime().addShutdownHook(Thread {
-//        println("\nProgram terminated.")
-//        exitProcess(0)
-//    })
-//
-//    println("Welcome to the To-Do List Application!")
-//    while (true) {
-//        print("> ")
-//        val input = scanner.nextLine()
-//
-//        when {
-//            input.equals("exit", ignoreCase = true) -> {
-//                println("Exiting...")
-//                break
-//            }
-//            input.startsWith("list notes", ignoreCase = true) -> {
-//                if (notes.isEmpty()) {
-//                    println("No notes available.")
-//                } else {
-//                    notes.forEachIndexed { index, note -> println("${index + 1}. $note") }
-//                }
-//            }
-//            input.startsWith("add note ", ignoreCase = true) -> {
-//                val note = input.removePrefix("add note ").trim()
-//                if (note.isNotEmpty()) {
-//                    notes.add(note)
-//                    println("Note added.")
-//                } else {
-//                    println("Note description cannot be empty.")
-//                }
-//            }
-//            else -> {
-//                println("Unknown command. Available commands: list notes, add note \"note description\", exit")
-//            }
-//        }
-//    }
-//    println("Goodbye!")
 }
